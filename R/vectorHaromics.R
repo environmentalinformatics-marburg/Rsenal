35f88f56053a2f2ac0b61600381616229e66a08e
fdetsch <florian.detsch@staff.uni-marburg.de>
2014-10-07 13:11:10 +0200

    Major update: added documentation to function `vectorHarmonics()`.

--- R/vectorHaromics.R
+++ R/vectorHaromics.R
@@ -1,8 +1,34 @@
+#' Fit harmonic trend model to time-series values 
+#' 
+#' @description This is a wrapper function around \code{\link{harmonic}} from 
+#' the \code{TSA} package that constructs harmonic functions for fitting a
+#' harmonic trend model. 
+#' 
+#' @param x Numeric. A vector containing time-series values.
+#' @param frq Integer, defaults to 12 (months). The number of observations per 
+#' unit of time. See \code{\link{ts}}. 
+#' @param st Numeric. A vector containing start year and month. 
+#' @param nd Numeric. A vector containing end year and month.
+#' @param m Integer, defaults to 2. The number of pairs of harmonic functions to
+#' be created. See \code{\link{harmonic}}.
+#' @param fun Aggregating function to be applied to the fitted values.
+#' 
+#' @return
+#' A numeric vector. 
+#' 
+#' @author
+#' Tim Appelhans
+#' 
+#' @seealso
+#' \code{\link{harmonic}}
+#' 
+#' @export vectorHarmonics
+#' @aliases vectorHarmonics
 vectorHarmonics <- function(x,
-                            frq = 12,
+                            frq = 12L,
                             st = c(1982, 1),
                             nd = c(1986, 12),
-                            m = 2,
+                            m = 2L,
                             fun = median) {
   
   stopifnot(require(TSA))
@@ -11,7 +37,7 @@ vectorHarmonics <- function(x,
   har <- harmonic(xts, m = m)
   mod <- lm(xts ~ har)
   fit <- ts(fitted(mod), start = st, end = nd, frequency = frq)
-  fit.med <- apply(matrix(fit, ncol = frq, byrow = T), 2, FUN = fun)
+  fit.med <- apply(matrix(fit, ncol = frq, byrow = TRUE), 2, FUN = fun)
   
   return(fit.med)
   
