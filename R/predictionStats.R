6a709d83c7515757648b0f3c7107b8c347abb293
tim-salabim <tim.appelhans@gmail.com>
2015-02-02 10:30:39 +0100

    UPDATE: minor cleaning of predictionStats()

--- R/predictionStats.R
+++ R/predictionStats.R
@@ -23,12 +23,21 @@
 #' Tim Appelhans
 #' 
 #' @examples
-#' pred_vals <- rnorm(1000, 200, 10)
-#' obs_vals <- rnorm(1000, 200, 10)
+#' ## create predictions with high accuracy (identical mean), 
+#' ## but low precision (sd double that of observations). Hence,
+#' ## ME should be close to zero and RMSE close to ten.
+#' pred_vals <- sort(rnorm(1000, 200, 20)) # sorting ensures high Rsq
+#' obs_vals <- sort(rnorm(1000, 200, 10))
 #' 
-#' result <- predictionStats(sort(pred_vals), sort(obs_vals), plot = TRUE)
+#' ## with plot = TRUE
+#' result <- predictionStats(pred_vals, obs_vals, plot = TRUE)
 #' result$stats
 #' result$plot
+#' 
+#' ## with plot = FALSE
+#' result <- predictionStats(pred_vals, obs_vals, 
+#'                           plot = FALSE, adj.rsq = FALSE)
+#' result
 #' 
 #' @export predictionStats
 #' @aliases predictionStats
@@ -54,14 +63,14 @@ predictionStats <- function(prd, obs, adj.rsq = TRUE, plot = FALSE) {
     nms <- names(df_all)[c(1, 3, 5)]
     
     rsq_plt <- dotplot("Rsq" ~ rsq, asp = 1, 
-                       xlab = "", ylab = "",
+                       xlab = "Value", ylab = "",
                        col = "grey20", xlim = c(-0.05, 1.05),
                        scales = list(x = list(alternating = 2,
                                               tck = c(1, 1))),
                        par.settings = envinmr.theme(),
                        cex = 1.2, as.table = TRUE) +
-      layer_(panel.abline(v = 1, lty = 3))
-
+      latticeExtra::layer_(panel.abline(v = 1, lty = 3, lwd = 1))
+    
     fit <- c(df_all$ME,
              df_all$MAE,
              df_all$RMSE)
@@ -75,19 +84,16 @@ predictionStats <- function(prd, obs, adj.rsq = TRUE, plot = FALSE) {
                          fit_se = fit_se)
     
     err_plt <- Hmisc::Dotplot(nms ~ Cbind(fit, 
-                                      fit + fit_se, 
-                                      fit - fit_se),
-                          data = df_plt, , asp = 1, 
-                          xlab = "Value", ylab = "",
-                          col = "grey20",  
-                          par.settings = envinmr.theme(),
-                          cex = 1.2, as.table = TRUE) + 
-      layer_(panel.lines(x = c(0, 0), 
-                        y = c(0, 3.25), 
-                        lty = 3, lwd = 1)) +
-      layer_(panel.lines(x = c(1, 1), 
-                         y = c(3.75, 4.25), 
-                         lty = 3, lwd = 1))
+                                          fit + fit_se, 
+                                          fit - fit_se),
+                              data = df_plt, , asp = 1, 
+                              xlab = "Value", ylab = "",
+                              col = "grey20",  
+                              par.settings = envinmr.theme(),
+                              cex = 1.2, as.table = TRUE) + 
+      latticeExtra::layer_(panel.lines(x = c(0, 0), 
+                                       y = c(0.75, 3.25), 
+                                       lty = 3, lwd = 1))
     
     err_plt <- update(err_plt, scales = list(tck = c(1, 1)))
     
@@ -95,8 +101,9 @@ predictionStats <- function(prd, obs, adj.rsq = TRUE, plot = FALSE) {
                                                layout = c(1, 2)), 
                             h = c(1/4, 3/4))
     out_plt$y.limits[[2]] <- nms[c(2, 1, 3)]
+    
   }
-
+  
   if (!plot) return(df_all) else 
     return(list(stats = df_all,
                 plot = out_plt))
