49501bb17f7daa606c1d704695f88602166bcc0d
tim-salabim <tim.appelhans@gmail.com>
2015-02-02 19:00:07 +0100

    UPDATE: predictionsStats() plotting function now properly drawing panel axes.

--- R/predictionStats.R
+++ R/predictionStats.R
@@ -55,21 +55,45 @@ predictionStats <- function(prd, obs, adj.rsq = TRUE, plot = FALSE) {
                        RMSE = sqrt(mean((prd - obs)^2, na.rm = TRUE)),
                        RMSE.se = se((prd - obs)^2),
                        Rsq = rsq)
-
+  
   if (plot) {
     library(latticeExtra)
-    library(Hmisc)
+    
+    ## panel.fun modified from 
+    ## http://thebiobucket.blogspot.de/2011/04/r-graphs-lattice-use-of-panel-functions.html
+    panel.fun <- function(...) {
+      if (panel.number() == 1) { 
+        at<-pretty(c(0, 1))
+        panel.axis("top", at = at, outside = FALSE,
+                   labels = TRUE, half = FALSE)
+        panel.abline(v = 1, lty = 3, lwd = 1)
+        panel.dotplot(..., lwd = 0.5)
+      }
+      if (panel.number() == 2) {
+        at <- pretty(rng)
+        panel.axis("bottom", at = at, outside = FALSE,
+                   labels = TRUE, half = FALSE)
+        panel.abline(v = 0, lty = 3, lwd = 1)
+        panel.lines(x = x_se[c(1, 4)], col = "grey60",
+                    y = y_se[c(1, 4)], lwd = 4)
+        panel.lines(x = x_se[c(2, 5)], col = "grey60",
+                    y = y_se[c(2, 5)], lwd = 4)
+        panel.lines(x = x_se[c(3, 6)], col = "grey60",
+                    y = y_se[c(3, 6)], lwd = 4)
+        panel.dotplot(..., lwd = 0.5)
+        panel.dotplot(x = df_plt$fit, y = df_plt$nms,
+                      cex = 1.3, col = "grey20", lwd = 0.5)
+      }
+    }
     
     nms <- names(df_all)[c(1, 3, 5)]
     
-    rsq_plt <- dotplot("Rsq" ~ rsq, asp = 1, 
+    rsq_plt <- dotplot("Rsq" ~ rsq, #asp = 0.5, 
                        xlab = "Value", ylab = "",
                        col = "grey20", xlim = c(-0.05, 1.05),
-                       scales = list(x = list(alternating = 2,
-                                              tck = c(1, 1))),
+                       scales = list(x = list(draw = FALSE)),
                        par.settings = envinmr.theme(),
-                       cex = 1.2, as.table = TRUE) +
-      latticeExtra::layer_(panel.abline(v = 1, lty = 3, lwd = 1))
+                       cex = 1.2, as.table = TRUE)
     
     fit <- c(df_all$ME,
              df_all$MAE,
@@ -81,27 +105,31 @@ predictionStats <- function(prd, obs, adj.rsq = TRUE, plot = FALSE) {
     
     df_plt <- data.frame(nms = nms,
                          fit = fit,
-                         fit_se = fit_se)
+                         fit_se = fit_se,
+                         upr = fit + fit_se,
+                         lwr = fit - fit_se)
     
-    err_plt <- Hmisc::Dotplot(nms ~ Cbind(fit, 
-                                          fit + fit_se, 
-                                          fit - fit_se),
-                              data = df_plt, , asp = 1, 
-                              xlab = "Value", ylab = "",
-                              col = "grey20",  
-                              par.settings = envinmr.theme(),
-                              cex = 1.2, as.table = TRUE) + 
-      latticeExtra::layer_(panel.lines(x = c(0, 0), 
-                                       y = c(0.75, 3.25), 
-                                       lty = 3, lwd = 1))
+    x_se <- c(df_plt$lwr, df_plt$upr)
+    y_se <- rep(c(2, 1, 3), 2)
     
-    err_plt <- update(err_plt, scales = list(tck = c(1, 1)))
+    rng <- range(x_se)
+    rng <- round(c(rng[1] - 0.2 * rng[1],
+                   rng[2] + 0.2 * rng[2]))
+    
+    err_plt <- dotplot(nms ~ upr + lwr,
+                       data = df_plt, , 
+                       xlab = "Value", ylab = "",
+                       col = "grey20",
+                       pch = "|",
+                       par.settings = envinmr.theme(),
+                       cex = 1.2, as.table = TRUE)
     
     out_plt <- resizePanels(latticeCombineGrid(list(rsq_plt, err_plt),
                                                layout = c(1, 2)), 
                             h = c(1/4, 3/4))
     out_plt$y.limits[[2]] <- nms[c(2, 1, 3)]
     
+    out_plt <- update(out_plt, panel = panel.fun)
   }
   
   if (!plot) return(df_all) else 
