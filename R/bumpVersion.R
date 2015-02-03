8cdeb97245313df9830ec247115cd3bf87f1fc12
tim-salabim <tim.appelhans@gmail.com>
2015-02-03 15:18:56 +0100

    UPDATE: included LICNENSE and modified bumpVersion to increase patch automatically

--- R/bumpVersion.R
+++ R/bumpVersion.R
@@ -16,45 +16,44 @@
 #' 
 #' @export bumpVersion
 #' 
-bumpVersion <- function(major, minor, patch, pkg.repo= ".") {
-  
-  new.ver <- paste(major, minor, patch, sep = ".")
+bumpVersion <- function(element = "patch", pkg.repo= ".") {
   
   ### DESCRIPTION file
   desc <- readLines(paste(pkg.repo, "DESCRIPTION", sep = "/"))
-  old.ver <- substr(desc[grep("Version*", desc)], 10, 14)
+  old.ver <- substr(desc[grep("Version*", desc)], 10, 
+                    nchar(desc[grep("Version*", desc)]))
   
-  new <- as.numeric(unlist(strsplit(new.ver, "\\.")))
   old <- as.numeric(unlist(strsplit(old.ver, "\\.")))
   
-  new <- new[1] * 100 + new[2] * 10 + new[3]
+  new.v <- switch(element,
+                  major = c(old[1] + 1, 0, 0),
+                  minor = c(old[1], old[2] + 1, 0),
+                  patch = c(old[1], old[2], old[3] + 1))
+  
+  new.ver <- paste(new.v[1], new.v[2], new.v[3], sep = ".")
+  
+  new.v <- new.v[1] * 100 + new.v[2] * 10 + new.v[3]
   old <- old[1] * 100 + old[2] * 10 + old[3]
   
-  if (old >= new) stop("new version number needs to be higher than old",
-                       "\n\n", "old version number is: ", old.ver)
-  else {
-    
-    desc[grep("Version*", desc)] <- paste("Version: ", new.ver, sep = "")
-    desc[grep("Date*", desc)] <- paste("Date:", Sys.Date(), sep = " ")
-    
-    writeLines(desc, paste(pkg.repo, "DESCRIPTION", sep = "/"))
-    
-    ### pkg.name-package.Rd file
-    pkg.name <- substr(desc[grep(glob2rx("Package:*"), desc)], 10, 
-                       nchar(desc[grep(glob2rx("Package:*"), desc)]))
-    
-    pkg.doc <- readLines(paste(pkg.repo, "man", 
-                               paste(pkg.name, "-package.Rd", sep = ""),
-                               sep = "/"))
-    
-    pkg.doc[grep("Version*", pkg.doc)] <- paste("Version: \\tab ", 
-                                                new.ver, "\\cr", sep = "")
-    pkg.doc[grep("Date*", pkg.doc)] <- paste("Date: \\tab ", 
-                                             Sys.Date(), "\\cr", sep = "")
-    
-    writeLines(pkg.doc, paste(pkg.repo, "man", 
-                              paste(pkg.name, "-package.Rd", sep = ""),
-                              sep = "/"))
-  }
-
+  desc[grep("Version*", desc)] <- paste("Version: ", new.ver, sep = "")
+  desc[grep("Date*", desc)] <- paste("Date:", Sys.Date(), sep = " ")
+  
+  writeLines(desc, paste(pkg.repo, "DESCRIPTION", sep = "/"))
+  
+  ### pkg.name-package.Rd file
+  pkg.name <- substr(desc[grep(glob2rx("Package:*"), desc)], 10, 
+                     nchar(desc[grep(glob2rx("Package:*"), desc)]))
+  
+  pkg.doc <- readLines(paste(pkg.repo, "man", 
+                             paste(pkg.name, "-package.Rd", sep = ""),
+                             sep = "/"))
+  
+  pkg.doc[grep("Version*", pkg.doc)] <- paste("Version: \\tab ", 
+                                              new.ver, "\\cr", sep = "")
+  pkg.doc[grep("Date*", pkg.doc)] <- paste("Date: \\tab ", 
+                                           Sys.Date(), "\\cr", sep = "")
+  
+  writeLines(pkg.doc, paste(pkg.repo, "man", 
+                            paste(pkg.name, "-package.Rd", sep = ""),
+                            sep = "/"))
 }
