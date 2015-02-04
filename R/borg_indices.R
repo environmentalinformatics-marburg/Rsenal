f79e14792e2f3159502e8810a1ed2b0df9996a35
Hanna <hanna.meyer1@gmail.com>
2015-02-04 15:37:12 +0100

    new function for calculation of shape parameters from rasters

---
+++ R/borg_indices.R
@@ -0,0 +1,55 @@
+#' Calculate shape indices listed in Borg 1998 
+#' @description used as internal funcction. see \code{?geometry_variables}
+#' and Borg 1998: Vergleichende Analyse von Formindizes zur Characterisierung 
+#' von Landschaftsobjekten unter ökologischen Aspekten. 
+#' Zeitschrift für Photogrammetrie und Fernerkundung 4: 108-119.
+#' @param Ar area of the entities
+#' @param De Diameter of the inner circle
+#' @param Du Diameter of the outer circle
+#' @return A RasterStack
+#' @author Hanna Meyer
+#' @seealso \code{?geometry_variables}
+
+
+borg_indices=function(Ar,Ur,De,Du){
+
+  Dk=sqrt((Ar/pi))*2 # Diameter of the circle of area Ar
+  
+  Uk=Dk*pi # perimeter of the circle of area Ar
+  Ue=De*pi #perimeter of the inner circle
+  Uu=Du*pi #perimeter of the outer circle
+
+  Au<-pi*((Du/2)^2) #area of outer circle
+  Ae=pi*((De/2)^2) #area of the inner circle
+
+  CI1<-Ue/Uk
+  CO1<-Uk/Uu
+  CI2<-(Uk-Ue)/Uk
+  CO2<-(Uu-Uk)/Uu
+  CCI1=(CI1+CO1)/2
+  CCI2=1-sqrt(CI2*CO2)
+  ### orientation at re circle of same area
+  CO<-(2*sqrt(pi*Ar))/Ur
+  SHD<-Ur/(2*sqrt(pi*Ar))
+  C1<-(4*Ar)/(Ur^2)
+  E<-Ur/(sqrt(Ar))
+  TR<-(4*pi*Ar)/(Ur^2)
+  ###orientation at the smallest outer circle
+  CR<-Ar/Au
+  C2 <- sqrt(Ar/Au)
+  FR<-Ar/Du^2
+  EI<-(pi*Du*(0.5*Du))/Ar
+  SF1<-Uu/Ur
+  GSI<-Ur/Du
+  ###orientation at the largest inner circle
+  SF2<-Ae/Ar
+  ###orientation on inner and outer circle
+  C3<-sqrt(De/Du)
+  SF3<-(Au-Ae)/Ar
+  
+  result=stack(CI1,CO1,CI2,CO2,CCI1,CCI2,CO,SHD,C1,E,TR,CR,
+               C2,FR,EI,SF1,GSI,SF2,C3,SF3)
+  names(result)=c("CI1","CO1","CI2","CO2","CCI1","CCI2","CO","SHD","C1","E",
+                  "TR","CR","C2","FR","EI","SF1","GSI","SF2","C3","SF3")
+  return(result)
+}
---
+++ R/geometry_variables.R
@@ -0,0 +1,101 @@
+#' Calculate selected geometry variables from raster data
+#' @description This function calculates geoemtry parameters from Raster data.
+#' @param x A rasterLayer with any values but where patches are seperated by surrounding NA values
+#' @return A RasterStacks containing the geoemtry parameters. The names are as following;
+#' Patches: The ID of the Patches
+#' Ar: The area of the individual patches
+#' SI: The shape index calculated by SDMTools
+#' CA: The core area calculated by SDMTools
+#' Up: The perimeter of the indivisual patches
+#' CAI: The core area index calculated by SDMTools
+#' PAR:The perimeter area ratio calculated by SDMTools
+#' Re: The radius of the inner circle
+#' distEdges: The distcance from edges 
+#' Ru: The radius of the outer circle
+#' OIC: The inner-outer-circle ratio
+#' CI1: inner compactness
+#' CO1: outer compactness
+#' CI2: inner compactness2
+#' CO2: outer compactness2
+#' CCI1: corrected compactness index 1
+#' CCI2: corrected compactness index 2
+#' CO: compactness
+#' SHD: Shoreline development
+#' C1: Circularity
+#' E: Elongateness
+#' TR:Thinness Ratio
+#' CR: Circularity ratio
+#' C2: Circularity2
+#' FR: Form ratio
+#' EI: Elipticity index
+#' SF1: Shape factor
+#' GSI: Grain shape index
+#' SF2: Shape factor 2
+#' C3: Circularity 3
+#' SF3: Shape factor 3
+#' 
+#' For decription of the indices see list in Borg 1998: Vergleichende Analyse von Formindizes zur Characterisierung 
+#' von Landschaftsobjekten unter ökologischen Aspekten. 
+#' Zeitschrift für Photogrammetrie und Fernerkundung 4: 108-119
+#' @author Hanna Meyer
+#' @seealso \code{SDMTools},  \code{clump}, \code{borg_indices}
+#' @examples
+#' RasterLayer<-raster("inst/training.rst")
+#' shape<-geometry.variables(RasterLayer)
+#' plot(shape)
+#' 
+
+geometry_variables <- function(x){
+     require(SDMTools)
+     require(raster)
+     Patches<-clump(x)
+     Stats=PatchStat(Patches)
+     # patch area:
+     Area <- reclassify(Patches, cbind(Stats$patchID,Stats$area))
+     #shape index:
+     shapeIndex <- reclassify(Patches, cbind(Stats$patchID,Stats$shape.index))
+     #core area:
+     coreArea <- reclassify(Patches, cbind(Stats$patchID,Stats$core.area.index))
+     #perimeter:
+     perimeter <- reclassify(Patches, cbind(Stats$patchID,Stats$perimeter))
+     #core.area.index
+     coreAreaIndex <- reclassify(Patches, cbind(Stats$patchID,Stats$core.area.index))
+     #the ratio of the patch perimeter (m) to area (m2)
+     perimAreaRatio <- reclassify(Patches, cbind(Stats$patchID,Stats$perim.area.ratio))
+     #distance to edge:
+     edges<-boundaries(Patches, type='inner')
+     distEdges<- gridDistance(edges,origin=1) 
+     values(distEdges)[is.na(values(Patches))]=NA
+     #innerCircle (largest circle)= maximum distance from edge
+     tmp=zonal(distEdges,Patches,fun="max")
+     innerCircle=Patches
+     innerCircle=reclassify(innerCircle,tmp)
+     ##### outer circle
+     oci=c()
+  
+     for (i in 1:max(values(Patches),na.rm=TRUE)){
+      cp=Patches
+      cp[cp!=i]=NA
+      cpp=rasterToPolygons(cp,dissolve=TRUE)
+      centroid=gCentroid(cpp, byid=TRUE,id=attributes(cpp)$plotOrder)
+      
+      dist<- distanceFromPoints(Patches, centroid)
+      dist[is.na(cp)]=NA
+      oci[i]=max(values(dist),na.rm=TRUE)
+     }
+     outerCircle <- reclassify(Patches, cbind(Stats$patchID,oci))
+     outerInnerCircle <- outerCircle-innerCircle
+
+  ### Indices listed and/or developed by Borg 98
+    borg<-borg_indices(Ar=Area,Ur=perimeter,De=innerCircle*2,Du=outerCircle*2)
+    
+  
+  ##############################################################################  
+     result<-stack(Patches,Area,shapeIndex,coreArea,perimeter,
+                   coreAreaIndex, perimAreaRatio,innerCircle,distEdges,outerCircle,
+                   outerInnerCircle,borg)
+     names(result)=c("Patches","Ar","SI","CA",
+                     "Up", "CAI","PAR",
+                     "Re","distEdges","Ru","OIC",names(borg))
+     return(result)  
+}
