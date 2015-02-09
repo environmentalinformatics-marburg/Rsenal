e5e0a14e76168e5aedf143c137dda9dc31b9e4ab
tim-salabim <tim.appelhans@gmail.com>
2015-02-09 17:59:58 +0100

    BUGFIX: histmatchRaster() had problems with NAs

--- R/histmatchRaster.R
+++ R/histmatchRaster.R
@@ -1,9 +1,9 @@
 #' histogram matching for RasterLayers
-#' 
-#' @description 
+#'
+#' @description
 #' this function is a wrapper around \code{\link{histmatch}} for
-#' RasterLayers. 
-#' 
+#' RasterLayers.
+#'
 #' @param x RasterLayer to be adjusted
 #' @param y target RasterLayer
 #' @param ttab logical, whether to return the transformation table
@@ -13,62 +13,75 @@
 #' (if \code{NULL}, the maximum of both layers)
 #' @param by step size used to build the new historgram
 #' (if \code{NULL}, 1 for integer master layer, 0.01 for double master layer)
-#' @param ... additional arguments (mask) passed to \code{\link{histmatch}} 
-#' 
+#' @param ... additional arguments (mask) passed to \code{\link{histmatch}}
+#'
 #' @return
-#' if \code{ttab = FALSE} a RasterLayer, if \code{ttab = TRUE} a list 
-#' with components 
+#' if \code{ttab = FALSE} a RasterLayer, if \code{ttab = TRUE} a list
+#' with components
 #' \code{recode} the trasnsformation table used to match the histograms
 #' \code{newraster} the transformed RasterLayer
-#' 
+#'
 #' @author
 #' Tim Appelhans
-#' 
+#'
 #' @seealso
 #' \code{\link{histmatch}}
-#' 
+#'
 #' @examples
+#' library(latticeExtra)
 #' tofix <- raster(matrix(rnorm(10000, 100, 3), nrow = 100, ncol = 100))
 #' master <- raster(matrix(rnorm(10000, 130, 7), nrow = 100, ncol = 100))
-#' 
+#'
 #' raw_p <- densityplot(tofix[], xlim = c(80, 160), plot.points = FALSE) +
-#'  as.layer(densityplot(master[], xlim = c(80, 160), 
+#'  as.layer(densityplot(master[], xlim = c(80, 160),
 #'           plot.points = FALSE, col = "red"))
-#' 
+#'
 #' raw_p
-#' 
+#'
 #' fixed <- histmatchRaster(tofix, master)
-#' raw_p + as.layer(densityplot(fixed[], xlim = c(80, 160), 
+#' raw_p + as.layer(densityplot(fixed[], xlim = c(80, 160),
 #'                  plot.points = FALSE, col = "black", lty = 2))
-#' 
+#'
 #' @export histmatchRaster
 #' @aliases histmatchRaster
 
-histmatchRaster <- function(x, y, 
-                            ttab = FALSE, 
-                            minval = NULL, 
-                            maxval = NULL, 
+histmatchRaster <- function(x, y,
+                            ttab = FALSE,
+                            minval = NULL,
+                            maxval = NULL,
                             by = NULL,
                             ...) {
-  
+
   library(landsat)
   library(raster)
-  
+  library(plyr)
+
   tbfxd <- as(x, "SpatialGridDataFrame")
   mstr <- as(y, "SpatialGridDataFrame")
+
+  if (is.null(minval)) {
+    minv <- floor(min(pmin(x[], y[]), na.rm = TRUE))
+    } else minv <- minval  
   
-  if (is.null(minval)) minv <- floor(min(pmin(x[], y[]))) else minv <- minval
-  if (is.null(maxval)) maxv <- ceiling(max(pmax(x[], y[]))) else maxv <- maxval
+  if (is.null(maxval)) {
+    maxv <- ceiling(max(pmax(x[], y[]), na.rm = TRUE))
+    } else maxv <- maxval
+
   if (is.null(by)) {
     step <- switch(typeof(x[]),
                    double = 0.01,
                    integer = 1L)
   } else step <- by
+
+  minv <- round_any(minv, step, f = floor)
+  maxv <- round_any(maxv, step, f = ceiling)
+  print(paste("min: ", minv))
+  print(paste("max: ", maxv))
   
-  fix <- histmatch(mstr, tbfxd, minval = minv, 
+  fix <- histmatch(mstr, tbfxd, minval = minv,
                    maxval = maxv, by = step, ...)
   fix_rst <- raster(fix$newimage)
-  
+
   if (ttab) {
     return(list(recode = fix$recode,
                 newraster = fix_rst))
@@ -76,4 +89,3 @@ histmatchRaster <- function(x, y,
     return(fix_rst)
   }
 }
-    
