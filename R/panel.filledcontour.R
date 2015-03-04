3f12a8fe21b25d6e0bb3c1bff7f2eb9821d95f0a
tim-salabim <tim.appelhans@gmail.com>
2015-03-05 00:40:07 +0100

    UPDATE: added new functionality to plot rasters as filled contours

---
+++ R/panel.filledcontour.R
@@ -0,0 +1,95 @@
+#' create smooth contour fills for lattice levelplots
+#' 
+#' @description 
+#' this panel function will create a smooth fill for a 
+#' \code{\link{levelplot}} similar to \code{\link{filled.contour}}
+#' 
+#' @param x vector
+#' @param y vector
+#' @param z vector
+#' @param subscripts optional 
+#' @param fill.contours should regions between contours be (colour) filled?
+#' @param at sequence for the fill contours
+#' @param contours should contour lines be drawn?
+#' @param at.contours sequence for the contour lines
+#' @param col.regions colour palette for filling
+#' @param col.contours colour of contour lines
+#' @param draw.labes should labels be drawn along the contour lines?
+#' @param ... currently not used
+#' 
+#' @author
+#' Tim Appelhans 
+#' (modified from: http://grokbase.com/t/r/r-help/089fhk4km2/r-creating-smooth-color-regions-with-panel-contourplot)
+#' 
+#' @examples
+#' library(latticeExtra)
+#' 
+#' data(volcano)
+#' 
+#' plot.new()
+#' levelplot(volcano, panel = Rsenal:::panel.filledcontour,
+#'           at.contour = seq(100, 200, 20))
+
+panel.filledcontour <- function(x, y, z, 
+                                subscripts,
+                                fill.contours = TRUE,
+                                at,
+                                contours = TRUE, 
+                                at.contours = NULL, 
+                                col.regions = cm.colors,
+                                col = col.regions(length(at) - 1),
+                                col.contours = "black",
+                                draw.labels = TRUE,
+                                ...) {
+  
+  library("gridBase")
+  library("plyr")
+  
+  z <- matrix(z[subscripts],
+              nrow = length(unique(x[subscripts])),
+              ncol = length(unique(y[subscripts])))
+  if (!is.double(z)) storage.mode(z) <- "double"
+  
+  opar <- par(no.readonly = TRUE)
+  on.exit(par(opar))
+  if (panel.number() > 1) par(new = TRUE)
+  par(fig = gridFIG(), omi = c(0, 0, 0, 0), mai = c(0, 0, 0, 0))
+  cpl <- current.panel.limits()
+  plot.window(xlim = cpl$xlim, ylim = cpl$ylim,
+              log = "", xaxs = "i", yaxs = "i")
+  
+  # paint the color contour regions
+  if (fill.contours) {
+  
+    fill_seq <- at
+    
+    .filled.contour(as.double(do.breaks(cpl$xlim, 
+                                        nrow(z) - 1)),
+                    as.double(do.breaks(cpl$ylim, 
+                                        ncol(z) - 1)),
+                    z, 
+                    levels = as.double(fill_seq), 
+                    col = col)
+    
+  } else NULL
+    
+  #add contour lines
+  if (contours) {
+    
+    if (is.null(at.contours)) {
+      cont_seq <- at
+    } else {
+      cont_seq <- at.contours
+    }
+    
+    contour(as.double(do.breaks(cpl$xlim, nrow(z) - 1)),
+            as.double(do.breaks(cpl$ylim, ncol(z) - 1)),
+            z, 
+            levels = as.double(cont_seq), 
+            add = TRUE,
+            col = col.contours, # color of the lines
+            drawlabels = draw.labels)
+    
+  } else NULL
+  
+}
---
+++ R/spFilledContour.R
@@ -0,0 +1,46 @@
+#' Plot rasters as filled contours
+#' 
+#' @description
+#' this function plots Raster* objects as filled contours
+#' 
+#' @param x a Raster* object
+#' 
+#' @seealso \code{\link{panel.filledcontour}}
+#' 
+#' @author
+#' Tim Appelhans
+#' 
+#' @examples
+#' library(raster)
+#' library(sp)
+#' 
+#' data(meuse.grid)
+#' coordinates(meuse.grid) = ~x+y
+#' proj4string(meuse.grid) <- CRS("+init=epsg:28992")
+#' gridded(meuse.grid) = TRUE
+#' 
+#' ## convert to raster
+#' meuse_rst <- raster(meuse.grid, layer = "dist")
+#' 
+#' ## standard spplot
+#' spplot(meuse_rst)
+#' 
+#' ## spFilledContour plot
+#' plot.new() # needed when using package 'gridBase' (used in panel.filledcontour)
+#' spFilledContour(x = meuse_rst)
+#' 
+#' 
+#' @export spFilledContour
+#' @aliases spFilledContour
+
+spFilledContour <- function(x, xlab = "", ylab = "", ...) {
+  
+  mat <- raster::as.matrix(flip(x, 2))
+
+  plot.new()
+  levelplot(t(mat), panel = Rsenal:::panel.filledcontour,
+            row.values = as.numeric(unique(coordinates(x)[, 1])),
+            column.values = as.numeric(unique(coordinates(x)[, 2])),
+            xlab = xlab, ylab = ylab, ...)
+  
+}
