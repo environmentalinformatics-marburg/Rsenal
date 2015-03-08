70769ff35b970aa08fce7c414a7036ff5c59b869
tim-salabim <tim.appelhans@gmail.com>
2015-03-08 01:37:31 +0100

    UPDATE: added panel.map() to draw maps on top of spplots. Very rudimentary!!

---
+++ R/panel.map.R
@@ -0,0 +1,41 @@
+#' panel function for maps
+#' 
+#' @description 
+#' this function produces a \code{\link{panel.polygon}} from a 
+#' \code{\link{map}} object. This can then be used to visualise 
+#' the map on top of an \code{\link{spplot}} object.
+#' NOTE: currently only tested for unprojected latlon data.
+#' 
+#' @param map.data the map type - see \code{\link{map}} database argument
+#' @param fill should the polygons be filled?
+#' @param col the colour of the fill
+#' @param ... additional arguments passed on to \code{\link{panel.polygon}} 
+#' 
+#' @return
+#' A \code{\link{panel.polygon}} object
+#' 
+#' @author
+#' Tim Appelhans
+#' 
+#' @examples
+#' library(latticeExtra)
+#' rst <- raster(ncols = 360, nrows = 180)
+#' rst[] <- rnorm(360 * 180)
+#' 
+#' spplot(rst) + layer(panel.map("world"))
+#' 
+#' @export panel.map
+#' @aliases panel.map
+
+panel.map <- function(map.data, fill = TRUE, col = "grey70", ...) {
+  
+  library(maps)
+  
+  mm <- map(map.data, plot = FALSE, fill = fill, 
+            col = col)
+  
+  pp <- panel.polygon(mm$x, mm$y, ...)
+  
+  return(pp)
+  
+}
