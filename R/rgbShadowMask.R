fe0f51060a915751d5bbb27c0d10ffebc2b2ba97
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-05-18 14:29:27 +0200

    Create shadow mask from RGB raster stack.

---
+++ R/rgbShadowMask.R
@@ -0,0 +1,73 @@
+#' Shadow mask from RGB (satellite) imagery
+#' 
+#' @description
+#' This function builds a shadow mask from a RGB \code{Raster*} object.  
+#' 
+#' @param rgb A \code{RasterStack} or \code{RasterBrick} object. 3
+#' bands are mandatory (usually red, green and blue).
+#' @param ... Further arguments passed on to \code{\link{rgb2YCbCr}}.
+#' 
+#' @return 
+#' A shadow mask \code{RasterLayer}.
+#' 
+#' @author
+#' Florian Detsch, Tim Appelhans
+#' 
+#' @references
+#' Deb, K. and Suny, A.H. (2014): Shadow Detection and Removal Based on YCbCr Color Space. Smart Computing Review 4, 23-33, doi:10.6029/smartcr.2014.01.003.
+#' 
+#' @seealso \code{\link{rgb2YCbCr}}
+#' 
+#' @examples
+#' data(gmap_hel)
+#' 
+#' plotRGB(gmap_hel)
+#' 
+#' gmap_hel_wsh <- rgbShadowMask(gmap_hel)
+#' plot(gmap_hel_wsh, axes = FALSE, legend = FALSE, 
+#'      col = rev(brewer.pal(9, "Greys")), alpha = .1, add = TRUE)
+#' 
+#' @export rgbShadowMask
+#' @aliases rgbShadowMask
+rgbShadowMask <- function(rgb, ...) {
+
+  ### prerequisites
+  
+  ## compatibility check
+  if (nlayers(rgb) < 3)
+    stop("Argument 'rgb' needs to be a Raster* object with at least 3 layers (usually red, green and blue).")
+  
+  ## 3-by-3 focal matrix
+  mat_w3by3 <- matrix(rep(1, 9), nc = 3)
+  
+  
+  ### processing
+  
+  ## rgb to ycbcr color space
+  rst_ycbcr <- rgb2YCbCr(rgb = rgb, ...)
+  
+  ## initial threshold
+  y <- rst_ycbcr[[1]]
+  num_mu_y <- mean(y[])
+  num_sd_y <- sd(y[])
+  num_trsh <- num_mu_y - num_sd_y
+  
+  ## rejection of focal means based on calculated threshold
+  rst_fcl_mu <- focal(y, w = mat_w3by3, fun = mean, na.rm = TRUE, pad = TRUE)
+  rst_fcl_mu[rst_fcl_mu[] < num_trsh] <- 0
+  rst_fcl_mu[rst_fcl_mu[] > 0] <- 1
+  
+  ## modified threshold
+  y_msk <- y[rst_fcl_mu[] == 0]
+  num_mu_y_msk <- mean(y_msk)
+  num_sd_y_msk <- sd(y_msk)
+  num_trsh_msk <- num_mu_y_msk - num_sd_y_msk
+  
+  ## rejection of focal means based on modified threshold
+  rst_fcl_mu_msk <- focal(y, w = mat_w3by3, fun = mean, na.rm = TRUE, pad = TRUE)
+  rst_fcl_mu_msk[rst_fcl_mu_msk[] < num_trsh_msk] <- 0
+  rst_fcl_mu_msk[rst_fcl_mu_msk[] > 0] <- 1
+  
+  ## return shadow mask
+  return(rst_fcl_mu_msk)
+}
