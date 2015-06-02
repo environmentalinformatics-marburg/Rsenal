5df58750628daea18788396c53d015987a1a3969
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-06-02 11:39:44 +0200

    Added documentation to `modeInteger` and bumped package version.

--- R/modeInteger.R
+++ R/modeInteger.R
@@ -1,3 +1,25 @@
+#' Calculate modal value from integer vector
+#'
+#' @description
+#' This function computes the modal value, i.e. the value that appears most 
+#' often, from an integer vector.
+#' 
+#' @param v Integer. The input vector.
+#' 
+#' @return
+#' The \code{integer} modal value.
+#' 
+#' @author
+#' Tim Appelhans
+#' 
+#' @examples
+#' set.seed(10)
+#' x <- sample(1:10, 100, replace = TRUE)
+#' 
+#' modeInteger(x)
+#' 
+#' @export modeInteger
+#' @aliases modeInteger
 modeInteger <- function(v) {
   a <- sort(table(v), decreasing = TRUE)
   r <- c()
