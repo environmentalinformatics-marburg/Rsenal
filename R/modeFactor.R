a386a452ad5dd4bbdc889cd8dc4ded0940512cd7
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-06-05 14:02:47 +0200

    Added documentation to `modeFactor`.

--- R/modeFactor.R
+++ R/modeFactor.R
@@ -1,3 +1,27 @@
+#' Calculate modal value from factor vector
+#'
+#' @description
+#' This function computes the modal value, i.e. the value that appears most 
+#' often, from a factor vector.
+#' 
+#' @param v Factor. The input vector.
+#' 
+#' @return
+#' The modal value as \code{character}.
+#' 
+#' @author
+#' Tim Appelhans
+#' 
+#' @examples
+#' ## sample data (flights departing from Houston airports)
+#' library(hflights)
+#' data(hflights)
+#' 
+#' ## most common flight number
+#' modeFactor(hflights$FlightNum)
+#'  
+#' @export modeFactor
+#' @aliases modeFactor
 modeFactor <- function(v) {
   a <- sort(table(v), decreasing = TRUE)
   r <- c()
