9d27c8e202dfa72f4de9b5726a8ca6501a0b537b
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-09-30 16:11:46 +0200

    Adjusted GIMMS download routine to updated online server structure.

--- R/downloadGimms.R
+++ R/downloadGimms.R
@@ -2,10 +2,12 @@
 #' 
 #' @description
 #' Download GIMMS 3G binary data for a given time span from NASA FTP server 
-#' (\url{http://ecocast.arc.nasa.gov/data/pub/gimms/3g/}).
+#' (\url{http://ecocast.arc.nasa.gov/data/pub/gimms/3g.v0/}).
 #' 
-#' @param decades Numeric. Decadal data to download. If not supplied, the entire
-#' GIMMS 3G dataset will be acquired, i.e. \code{decades = seq(1980, 2010, 10)}.
+#' @param begin Numeric. Start year, e.g. '2000'. If not supplied, download will 
+#' start from the first year available.
+#' @param end Numeric. End year. If not supplied, download will stop at the last 
+#' year available.
 #' @param dsn Character. Destination folder for file download. If not supplied, 
 #' all downloaded files will be stored in the current working directory. 
 #' @param ... Further arguments. Currently not in use. 
@@ -21,51 +23,44 @@
 #' 
 #' @examples  
 #' # Download GIMMS 3G data from 1990 to 2009 (this might take some time...)
-#' downloadGimms(decades = c(1990, 2000), 
-#'               dsn = paste(getwd(), "data", sep = "/"))
+#' downloadGimms(begin = 2000, dsn = paste(getwd(), "data", sep = "/"))
 #'               
 #' @export downloadGimms
 #' @aliases downloadGimms
-downloadGimms <- function(decades, 
+downloadGimms <- function(begin = NULL, end = NULL,
                           dsn = ".",
                           ...) {
   
-  # URL of GIMMS 3G FTP server
-  gimms_ftp <- "http://ecocast.arc.nasa.gov/data/pub/gimms/3g/"
+  #   # URL of GIMMS 3G FTP server
+  #   gimms_ftp <- "http://ecocast.arc.nasa.gov/data/pub/gimms/3g.v0/"
   
-  # Loop through decades
-  gimms_fls <- lapply(decades, function(yr) {
-    # Download list of avl files from current download directory
-    tmp_dir <- paste0(gimms_ftp, yr, "s_new")
-    download.file(tmp_dir, 
-                  destfile = paste0(dsn, "/", basename(tmp_dir), ".txt"))
-    
-    # Extract and import avl files from downloaded list
-    lns <- readLines(paste0(dsn, "/", basename(tmp_dir), ".txt"))
-    id <- which(substr(lns, 1, 4) == "<img")
-    lns <- lns[id]
-    
-    # Download avl files if not present in `dsn`
-    tmp_fls <- substr(lns, 53, 72)
-    tmp_url <- paste(tmp_dir, tmp_fls, sep = "/")
-    
-    for (i in tmp_url) {
-      destfile <- paste0(dsn, "/", basename(i))
-      if (file.exists(destfile)) {
-        cat("File", destfile, "already exists in destination folder. Proceeding to next file ...\n")
-      } else {
-        try(download.file(i, destfile = destfile), silent = TRUE)
-      }
+  ## available files
+  gimms_fls <- readLines("http://ecocast.arc.nasa.gov/data/pub/gimms/3g.v0/00FILE-LIST.txt")
+  
+  ## if specified, subset available files by time frame
+  gimms_bsn <- basename(gimms_fls)
+  gimms_yrs_chr <- substr(gimms_bsn, 4, 5)
+  gimms_yrs_num <- as.numeric(gimms_yrs_chr)
+  gimms_yrs_chr[gimms_yrs_num > 80] <- paste0("19", gimms_yrs_chr[gimms_yrs_num > 80])
+  gimms_yrs_chr[gimms_yrs_num <= 80] <- paste0("20", gimms_yrs_chr[gimms_yrs_num <= 80])
+  gimms_yrs_num <- as.numeric(gimms_yrs_chr)
+  
+  if (is.null(begin)) begin <- gimms_yrs_num[1]
+  if (is.null(end)) end <- gimms_yrs_num[length(gimms_yrs_num)]
+  gimms_fls <- gimms_fls[gimms_yrs_chr %in% seq(begin, end)]
+  
+  ## download
+  for (i in gimms_fls) {
+    destfile <- paste0(dsn, "/", basename(i))
+    if (file.exists(destfile)) {
+      cat("File", destfile, "already exists in destination folder. Proceeding to next file ...\n")
+    } else {
+      try(download.file(i, destfile = destfile), silent = TRUE)
     }
-    
-    # Remove list of avl files
-    file.remove(paste0(dsn, "/", basename(tmp_dir), ".txt"))
-    
-    tmp_fls <- paste0(dsn, "/", tmp_fls)
-    return(tmp_fls)
-  })
+  }
   
-  gimms_fls <- unlist(gimms_fls)
-  return(gimms_fls)
+  ## return vector with output files
+  gimms_out <- paste0(dsn, "/", gimms_fls)
+  return(gimms_out)
   
 }
