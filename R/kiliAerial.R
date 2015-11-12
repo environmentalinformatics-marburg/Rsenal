a714654a58795a49a62b29edd647d71dccda7eb9
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-11-12 14:59:36 +0100

    Function now works with 'upperLeft' and 'lowerRight' bounding box coordinates specified; if not supplied and 'template' is missing, the 8-km GIMMS grid covering Mt. Kilimanjaro is imported.

--- R/kiliAerial.R
+++ R/kiliAerial.R
@@ -1,25 +1,28 @@
 #' Retrieve an (aerial) image of Mt. Kilimanjaro
 #' 
 #' @description
-#' Based on the functionalities offered by the R \bold{OpenStreetMap} package, 
+#' Based on functionalities offered by the R \bold{OpenStreetMap} package, 
 #' this function per default retrieves a Bing aerial image centered on Mt. 
-#' Kilimanjaro, Tanzania, that is aligned with the 8-km GIMMS grid covering the
-#' region.
+#' Kilimanjaro, Tanzania, that is aligned with the 8-km GIMMS NDVI grid covering 
+#' the region.
 #' 
-#' @param projection Character, defaults to "+init=epsg:21037". Output projection
-#' of the retrieved OSM data, passed on to \code{\link{openproj}}.
-#' @param type Character, defaults to "bing". Tile server from which to get the
+#' @param upperLeft,lowerRight 'numeric'. Upper-left (lower-right) bounding box 
+#' coordinates passed on to \code{\link{openmap}}. If not supplied and 
+#' 'template' is missing, the returned image defaults to the extent of the study 
+#' area covered in \strong{Detsch et al. (under review)}.
+#' @param template 'Extent' object, or any object that is compatible with 
+#' \code{\link{extent}}. 
+#' @param projection 'character', defaults to "+init=epsg:21037". Output projection
+#' of the retrieved image, see \code{\link{openproj}}.
+#' @param type 'character', defaults to "bing". Tile server from which to get the
 #' map, see \code{\link{openmap}}. 
-#' @param template \code{Extent} object, or any object from which an \code{Extent}
-#' object can be extracted. Defaults to the extent of the 8-km GIMMS grid used 
-#' in \strong{Detsch et al. (in preparation)}.
-#' @param rasterize Logical, defaults to FALSE. Determines whether a multi-layer 
-#' \code{Raster*} object is returned.
+#' @param rasterize 'logical'. If \code{TRUE} (default), the function returns a 
+#' RGB 'RasterStack' object.
 #' @param ... Further arguments passed on to \code{\link{openmap}}. 
 #' 
 #' @return
 #' An object of class \code{OpenStreetMap} or, if \code{rasterize = TRUE}, 
-#' a \code{RasterStack} object.
+#' a multi-layer 'RasterStack' object.
 #' 
 #' @author
 #' Florian Detsch
@@ -27,18 +30,25 @@
 #' @seealso
 #' \code{\link{openmap}}, \code{\link{openproj}}
 #' 
-#' @examples  
-#' library(ggplot2)
+#' @references 
+#' Detsch, F., Otte, I., Appelhans, T., and T. Nauss (2015): Seasonal and 
+#' long-term vegetation dynamics from NDVI time series at Mt. Kilimanjaro, 
+#' Tanzania. Under review at \emph{Remote Sensing of Environment}.
 #' 
-#' img <- kiliAerial(minNumTiles = 20)
+#' @examples  
+#' ## download high-resolution image
+#' img <- kiliAerial(minNumTiles = 12L, rasterize = FALSE)
+#' 
+#' ## create figure
+#' library(ggplot2)
 #' autoplot(img)
 #'               
 #' @export kiliAerial
 #' @aliases kiliAerial
-kiliAerial <- function(projection = "+init=epsg:21037", 
+kiliAerial <- function(upperLeft, lowerRight, template = NULL, 
+                       projection = "+init=epsg:21037", 
                        type = "bing",
-                       template = NULL, 
-                       rasterize = FALSE,
+                       rasterize = TRUE,
                        ...) {
  
   # packages
@@ -46,15 +56,15 @@ kiliAerial <- function(projection = "+init=epsg:21037",
   jnk <- sapply(lib, function(x) stopifnot(require(x, character.only = TRUE)))
   
   # gimms extent
-  if (is.null(template)) {
-    template <- extent(c(36.99033, 37.74099, -3.415092, -2.83096))
-  } else {
-    template <- extent(template)
-  }
+  if (is.null(template) & (missing("upperLeft") | missing("lowerRight")))
+    template <- raster::extent(readRDS(system.file("extdata/gimms_grid.rds", 
+                                                   package = "Rsenal")))
   
   # data retrieval
-  kili.map <- openproj(openmap(upperLeft = c(ymax(template), xmin(template)), 
-                               lowerRight = c(ymin(template), xmax(template)), 
+  if (missing("upperLeft")) upperLeft <- c(ymax(template), xmin(template))
+  if (missing("lowerRight")) lowerRight <- c(ymin(template), xmax(template))
+  
+  kili.map <- openproj(openmap(upperLeft = upperLeft, lowerRight = lowerRight, 
                                type = type, ...), 
                        projection = projection)
   
