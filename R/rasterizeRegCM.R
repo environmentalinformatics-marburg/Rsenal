1e44fbf0ecbfcbfe18d46da9032592c9c50e73c0
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-02-03 07:55:46 +0100

    Removed `require` call.

--- R/rasterizeRegCM.R
+++ R/rasterizeRegCM.R
@@ -1,21 +1,21 @@
 #' Rasterize RegCM outout
-#' 
+#'
 #' @description
-#' The output of RegCM data are (amongst others) a rasters containing the 
-#' latitude and a longitude coordinates as well as raster of the data values. 
-#' All raster, however, are not projected but only in column/rows. A simple 
+#' The output of RegCM data are (amongst others) a rasters containing the
+#' latitude and a longitude coordinates as well as raster of the data values.
+#' All raster, however, are not projected but only in column/rows. A simple
 #' assignment of the lat/lon coordinates to the data is not possible due to
 #' the irregular spacing between the lat/lon coordinates. Therefore, the data
 #' must first be converted to spatial points and projected to a metric reference
-#' system. The values can then be rasterized by creating a template raster of 
-#' the extent and proj4string of the projected points as well as either 
-#' (if known) the resolution of the target raster in meter or (if resolution 
+#' system. The values can then be rasterized by creating a template raster of
+#' the extent and proj4string of the projected points as well as either
+#' (if known) the resolution of the target raster in meter or (if resolution
 #' unknown) the number of columns and rows from the original rasters.
 #' Note that often Model data need to be rotated and transposed.
-#' 
+#'
 #' The function also works for other data which are not projected but which have
 #' a latitude and a longitude raster as additional information.
-#' 
+#'
 #' @param x A Raster layer or raster stack containing the values
 #' @param lat A raster layer containing the latitude coordinates as pixel values
 #' @param lon A raster layer containing the longitude coordinates as pixel values
@@ -23,17 +23,16 @@
 #' @param transpose (requires documentation)
 #' @param rotate (requires documentation)
 #' @param targetcrs EPSG code or proj4String of the metric target reference system
-#' 
+#'
 #' @return
 #' A \code{RasterLayer} or \code{RasterStack} object.
-#' 
+#'
 #' @author
 #' Hanna Meyer and Tim Appelhans
 
 #' @export rasterizeRegCM
 #' @aliases rasterizeRegCM
 rasterizeRegCM <- function(x, lat, lon, res=NULL, transpose=TRUE, rotate =c("x","y"), targetcrs="+init=epsg:3031"){
-  require(raster)
   datstack <- stack(x,lat,lon)
   if (transpose){
     datstack <- t(datstack)
