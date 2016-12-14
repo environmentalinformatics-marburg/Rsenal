3dd44ab0c20641befd837282e1e0de93837bfe10
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-12-14 11:27:52 +0100

    Update: enabled rasterization of TRMM .HDF data.

--- R/rasterizeTRMM.R
+++ R/rasterizeTRMM.R
@@ -1,88 +1,59 @@
-#' Rasterize TRMM 3B42 binary data
+#' Rasterize TRMM 3B42 Data
 #' 
 #' @description
-#' Rasterize TRMM 3B42 daily binary data, e.g. previously downloaded via 
-#' \code{\link{downloadTRMM}}.
+#' Rasterize TRMM 3B42 .bin or .HDF files.
 #' 
-#' @param binary Character. TRMM binary file (*.bin). 
-#' @param meta Character. Corresponding metadata file (*.xml). 
-#' @param write_out Logical. Enables output storage.
+#' @param x \code{character}. TRMM 3B42 file(s). Currently supported filetypes 
+#' are .bin and .HDF. Note that \code{\link{rasterizeTRMMbin}} takes one file 
+#' only.
+#' @param ... Additional arguments passed to \code{\link{rasterizeTRMMbin}} or 
+#' \code{\link{rasterizeTRMMhdf}}.
 #' 
 #' @return
-#' A \code{RasterLayer} object.
+#' \itemize{
+#' \item{\strong{.bin files}: A single \code{RasterLayer} object.}
+#' \item{\strong{.HDF files}: A \code{list} of \code{Raster*} objects.}
+#' }
 #' 
 #' @author
 #' Florian Detsch
 #' 
 #' @seealso
-#' \code{\link{readBin}}, \code{\link{downloadTRMM}}
-#' 
-#' @references
-#' \url{https://stat.ethz.ch/pipermail/r-sig-geo/attachments/20110913/5ece92b0/attachment.pl}
+#' \code{\link{downloadTRMM}}, \code{\link{readBin}}, 
+#' \code{\link{rasterizeTRMMbin}}, \code{\link{rasterizeTRMMhdf}}.
 #' 
 #' @examples  
 #' \dontrun{
-#' ## download TRMM 3B42 data from Jan 1 to Jan 5, 2015
-#' fls_trmm <- downloadTRMM(begin = "2015-01-01", end = "2015-01-05")
+#' ## download 3-hourly TRMM 3B42 data from Jan 1, 2015
+#' fls <- downloadTRMM(begin = "2015-01-01", end = "2015-01-01", 
+#'                     type = "3-hourly")
+#' fls[1]                     
 #' 
 #' ## rasterize
-#' rst_trmm <- rasterizeTRMM(binary = fls_trmm[1, 1], meta = fls_trmm[1, 2], 
-#'                           write_out = FALSE)
-#'                             
-#' plot(rst_trmm)                                     
+#' library(rworldmap)
+#' eth <- subset(countriesCoarse, ADMIN == "Ethiopia")
+#' rst <- rasterizeTRMM(fls, ext = eth)
+#' 
+#' ## visualize
+#' plot(rst[[1]])
 #' }
-#'               
+#' 
 #' @export rasterizeTRMM
-#' @aliases rasterizeTRMM
-rasterizeTRMM <- function(binary, meta, write_out = TRUE) {
+#' @name rasterizeTRMM
+rasterizeTRMM <- function(x, ...) {
   
-  ## import metadata
-  ch_meta <- readLines(meta)
+  filetype <- strsplit(x, "\\.")  
+  filetype <- unique(sapply(filetype, "[[", length(filetype[[1]])))
   
-  ## extract bounding box
-  num_bbox <- sapply(c("WestBoundingCoordinate", "NorthBoundingCoordinate", 
-           "EastBoundingCoordinate", "SouthBoundingCoordinate"), 
-         function(i) {
-           
-           tmp_int_id <- grep(i, ch_meta)
-           tmp_ch_crd <- ch_meta[tmp_int_id]
-           
-           tmp_ch_crd <- gsub(" ", "", tmp_ch_crd)
-           tmp_ch_crd <- gsub("/", "", tmp_ch_crd)
-           tmp_ls_crd <- strsplit(tmp_ch_crd, paste0("<", i, ">"))
-           tmp_ch_crd <- unlist(tmp_ls_crd)[[2]]
-           tmp_ls_crd <- strsplit(tmp_ch_crd, paste0("</", i, ">"))
-           tmp_ch_crd <- unlist(tmp_ls_crd)
-           tmp_num_crd <- as.numeric(tmp_ch_crd)
-           
-           return(tmp_num_crd)                          
-         })
-
-  ## number of rows and columns
-  int_ncols <- (num_bbox[3] - num_bbox[1]) / 0.25
-  int_nrows <- (num_bbox[2] - num_bbox[4]) / 0.25
+  if (length(filetype) > 1)
+    stop("Please make sure all files in 'x' are of the same filetype.\n")
   
-  ## build world grid
-  rst_trmm <- raster(nrows = int_nrows, ncols = int_ncols, 
-                     xmn = num_bbox[1]-180, xmx = num_bbox[3]-180, 
-                     ymn = num_bbox[4], ymx = num_bbox[2])
-  rst_trmm <- setValues(rst_trmm, 0)
-  
-  ## import binary rainfall data
-  num_val <- readBin(binary, "double", n = int_ncols * int_nrows, size = 4, 
-                     endian = "big")
-  
-  ## insert values into raster template and save
-  ## (taken from https://stat.ethz.ch/pipermail/r-sig-geo/attachments/20110913/5ece92b0/attachment.pl)
-  rst_trmm[] <- num_val
-  
-  ch_fls_out <- gsub("\\.", "_", basename(binary))
-  ch_fls_out <- paste(dirname(binary), substr(ch_fls_out, 1, nchar(ch_fls_out)-4), 
-                      sep = "/")
-  
-  rst_trmm <- flip(rst_trmm, direction = "y", 
-                   filename = ifelse(write_out, ch_fls_out, ""), 
-                   format = "GTiff", overwrite = TRUE)
-  
-  return(rst_trmm)
+  if (filetype %in% c("HDF", "hdf")) {
+    rasterizeTRMMhdf(x, ...)
+  } else if (filetype %in% c("BIN", "bin")) {
+    rasterizeTRMMbin(x, ...)
+  } else {
+    stop("Provided filetype currently not supported, see ?rasterizeTRMM. \n")
+  }
 }
+
