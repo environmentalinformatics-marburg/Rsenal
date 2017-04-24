7091d563428b6a38fb1380dd6c25afa910d2f155
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-04-24 17:10:07 +0200

    Revised function, moved 'MODIS' to list of CRAN packages.

--- R/installEIPkgs.R
+++ R/installEIPkgs.R
@@ -1,32 +1,38 @@
-#' download and install packages for envin-umr
+#' Download and install packages for envin-umr
 #' 
 #' @description 
-#' download and install a bunch of packages that we find useful for our 
-#' day-to-day workflow. If \code{cran.pkgs = NULL} a standard list of 
-#' packages will be downloaded and installed. If you supply a character
+#' Download and install a bunch of packages that we find useful for our 
+#' day-to-day workflow. If \code{cran.pkgs = NULL} (default), a standard list of 
+#' packages will be downloaded and installed. If you supply a \code{character}
 #' vector of packages, these wil be installed instead. 
 #' 
-#' @param cran.pkgs character vector of packages to be installed from CRAN
-#' @param git.pkgs character vector of packages to be installed from GitHub (not yet implemented)
-#' @param check logical, whether to check if any packages specified in \code{cran.pkgs}
-#' are already installed and skip their installation
-#' @param update logical, whether to run \code{update.packages()} after installation
-#' @param MODIS logical, if TRUE MODIS package will be installed from r-forge
-#' @param dep logical, whether dependencies should be onstalled for specified packages
-#' @param ... Currently not used.
+#' @param cran.pkgs \code{character} vector of packages to be installed from 
+#' CRAN.
+#' @param git.pkgs \code{character} vector of packages to be installed from 
+#' GitHub (not yet implemented).
+#' @param check \code{logical}, determines whether to check if any packages 
+#' specified in 'cran.pkgs' are already installed and skip their installation.
+#' @param update \code{logical}, determines whether to run 
+#' \code{\link{update.packages}} after installation.
+#' @param ... Additional arguments passed to \code{\link{install.packages}}.
 #' 
 #' @author
-#' Tim Appelhans
+#' Tim Appelhans, Florian Detsch
+#' 
+#' @seealso 
+#' \code{\link{install.packages}}, \code{\link{install_github}}.
+#' 
+#' @examples 
+#' \dontrun{
+#' installEIPkgs()
+#' }
 #' 
 #' @export installEIPkgs
-#' 
-
+#' @name installEIPkgs
 installEIPkgs <- function(cran.pkgs = NULL, 
                           git.pkgs = NULL, 
                           check = TRUE,
                           update = FALSE,
-                          MODIS = FALSE,
-                          dep = TRUE,
                           ...) {
   
   ## environmental informatics package list if no pkgs supplied
@@ -42,27 +48,22 @@ installEIPkgs <- function(cran.pkgs = NULL,
                    "leaflet", "mapview", "RNiftyReg", "png", "jpeg", 
                    "jsonlite", "foreach", "gimms", "satellite", "htmltools", 
                    "lubridate", "modiscloud", "MODISTools", "grid", "ncdf", 
-                   "ncdf4", "OpenStreetMap", "party", "randomForest",
+                   "ncdf4", "OpenStreetMap", "party", "randomForest", "MODIS",
                    "rasterVis", "RColorBrewer", "Rcpp", "remote", "rgl", 
                    "spacetime", "spatial.tools", "stargazer", "stringr", 
                    "TSA", "vcd", "xts", "zoo", "bfast", "httpuv", "shiny") 
   }
   
-  if (check) {
-    chk <- as.character(installed.packages()[, 1])
-    '%!in%' <- function(x, y) !('%in%'(x, y))
-    ind <- which(cran.pkgs %!in% chk)
-  } else {
-    ind <- 1:length(cran.pkgs)
-  }
+  ## install cran packages (if required)
+  jnk <- lapply(cran.pkgs, function(i) {
+    install <- if (check) {
+      !(i %in% installed.packages()[, 1])
+    } else TRUE
+    
+    if (install) install.packages(i, ...)
+  })
   
-  
-  lapply(cran.pkgs[ind], function(i) install.packages(i, dependencies = dep))
-  
+  ## update cran packages
   if (update) update.packages()
-  
-  if (MODIS) {
-    install.packages("MODIS", repos="http://R-Forge.R-project.org")
-  }
+  return(invisible())
 }
-  
