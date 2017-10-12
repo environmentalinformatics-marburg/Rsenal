ef0a63e65341641d4b8a0cec24e38e0a50f09485
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-10-12 18:07:44 +0200

    Update: added documentation for stextGrob().

--- R/stextGrob.R
+++ R/stextGrob.R
@@ -1,59 +1,53 @@
-##' shadow text
-##'
-##' adds a blurred white version of a label below the text
-##' @title stextGrob
-##' @aliases stextGrob grid.stext
-##' @param label see textGrob
-##' @param r blur radius
-##' @param x see textGrob
-##' @param y see textGrob
-##' @param just see textGrob
-##' @param hjust see textGrob
-##' @param vjust see textGrob
-##' @param rot see textGrob
-##' @param check.overlap see textGrob
-##' @param default.units see textGrob
-##' @param name see textGrob
-##' @param gp see textGrob
-##' @param vp see textGrob
-##' @return gTree
-##' @seealso \url{https://github.com/baptiste/gridextra/blob/2c935bf9ec6d6d2a77cb0de2180b5e085d26ef39/R/stextGrob.r} 
-##' (accessed on January 28, 2016).
-##' @author Baptiste Auguie
-##' @family grob userlevel
-##' @export
-##' @examples
-##' library(grid)
-##' grid.newpage()
-##' grid.rect(gp=gpar(fill="grey"))
-##' grid.stext("test")
-stextGrob <- function (label, r=0.1, x = unit(0.5, "npc"), y = unit(0.5, "npc"), 
-                       just = "centre", hjust = NULL, vjust = NULL, rot = 0, check.overlap = FALSE, 
-                       default.units = "npc", name = NULL, gp = gpar(), vp = NULL){
+#' Draw Shadow Text
+#'
+#' @description 
+#' Create and draw shadow text by wrapping a textual expression into a colored 
+#' framing.
+#' 
+#' @param label A \code{character} or \code{\link{expression}} vector, see 
+#' \code{\link{textGrob}}.
+#' @param x,y Horizontal and vertical text position as \code{\link{unit}} 
+#' objects passed to \code{\link{grid.text}}.
+#' @param r Blur radius of colored framing as \code{numeric}.
+#' @param col,fill Framing and fill color passed to \code{\link{gpar}}.
+#' @param name,gp,vp Graphical parameters passed to \code{\link{gTree}}.
+#' @param ... Additional arguments passed to \code{\link{grid.text}}.
+#' 
+#' @return A text grob created by \code{\link{gTree}}.
+#' 
+#' @seealso \code{\link{grid.text}}.
+#' 
+#' @references \url{https://github.com/baptiste/gridextra/blob/2c935bf9ec6d6d2a77cb0de2180b5e085d26ef39/R/stextGrob.r} 
+#' (accessed on January 28, 2016).
+#' @author Baptiste Auguie, Florian Detsch
+#' @family grob userlevel
+#' 
+#' @examples
+#' library(grid)
+#' grid.newpage()
+#' grid.rect(gp = gpar(fill = "grey"))
+#' grid.stext("test")
+#' 
+#' @export
+#' @aliases stextGrob grid.stext 
+stextGrob <- function(label, x = unit(0.5, "npc"), y = unit(0.5, "npc")
+                      , col = "white", fill = "black", r = 0.1 
+                      , gp = gpar(), vp = NULL, name = NULL, ...) {
   
   let <- textGrob("a")
   
-  tg <- textGrob(label=label, x=x, y=y, gp=gpar(col="black"),
-                 just = just, hjust = hjust, vjust = vjust, rot = rot,
-                 check.overlap = check.overlap, 
-                 default.units = default.units)
+  tg <- textGrob(label, x, y, gp = gpar(col = fill), ...)
   
-  tgl <- c(lapply(seq(0, 2*pi, length=18), function(theta){
-    
-    textGrob(label=label,x=x+cos(theta)*r*grobWidth(let),
-             y=y+sin(theta)*r*grobHeight(let), gp=gpar(col="white"),
-             just = just, hjust = hjust, vjust = vjust, rot = rot,
-             check.overlap = check.overlap, 
-             default.units = default.units)
-    
+  tgl <- c(lapply(seq(0, 2 * pi, length = 18), function(theta) {
+    textGrob(label, x + cos(theta) * r * grobWidth(let),
+             y + sin(theta) * r * grobHeight(let), gp = gpar(col = col), ...)
   }), list(tg))
   
   
-  g <- gTree(children=do.call(gList, tgl), vp=vp, name=name, gp=gp)
-  
+  gTree(children = do.call(gList, tgl), vp = vp, name = name, gp = gp)
 }
 
-##' @export
+#' @export
 grid.stext <- function(...){
   g <- stextGrob(...)
   grid.draw(g)
