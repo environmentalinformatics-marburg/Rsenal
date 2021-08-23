fa513919578fee5c4fe8a7d0891a3c44f12dfa3f
fdetsch <fdetsch@web.de>
2021-08-23 10:46:42 +0200

    adds \doi{}

--- R/kiliAerial.R
+++ R/kiliAerial.R
@@ -36,8 +36,8 @@
 #' @references 
 #' Detsch F, Otte I, Appelhans T, Hemp A, Nauss T (2016) Seasonal and long-term 
 #' vegetation dynamics from 1-km GIMMS-based NDVI time series at Mt. 
-#' Kilimanjaro, Tanzania. Remote Sensing of Environment 178: 70-83. Available 
-#' online at \url{http://dx.doi.org/10.1016/j.rse.2016.03.007}.
+#' Kilimanjaro, Tanzania. Remote Sensing of Environment 178: 70-83, 
+#' \doi{10.1016/j.rse.2016.03.007}.
 #' 
 #' @examples  
 #' \dontrun{
